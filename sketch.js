9be78f2821cbd974beccdbb7265bf3081ed21116
SindhuPriyaa <74706216+SindhuPriyaa@users.noreply.github.com>
2021-05-28 19:57:59 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,28 @@
+var  database;
+var form, game, player;
+var gameState = 0;
+var playerCount;
+
+
+function setup(){
+  database = firebase.database();
+  createCanvas(500,500);
+
+  game = new Game();
+  game.getState();
+  game.start();
+  
+}
+
+function draw(){
+  if(playerCount ===4){
+    game.update(1);
+  }
+  if(gameState === 1){
+    game.play();
+  }
+  
+  
+    
+}
+
